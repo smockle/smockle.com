dfa309fb334c15a4d0415486a6b2cc2cf388aaed
Clay Miller <clay@smockle.com>
2015-11-06 08:22:07 -0800

    Update resume.

--- app/components/resume.jsx
+++ app/components/resume.jsx
@@ -20,18 +20,19 @@ class Resume extends React.Component {
     return (
       <div className={css.home.home}>
       <section className={css.resume.hero} id='hero'>
-        <div className={css.app.wrapper}>
+        <div className={css.resume.wrapper}>
           <header className={css.resume.header}>
-            <h1>Clay Miller</h1>
+            <h1 className={css.resume.big}>Clay Miller</h1>
             <div>
-
-              <span style={{margin: '0 10px'}}>/</span>
-              
+              <a href="mailto:clay@smockle.com">clay@smockle.com</a>
             </div>
             <p>I build the Internet. My ideal workplace lets me take ownership in my work, learn bleeding-edge technologies, help people connect and grow, and build things that endure. Diversity, accessibility and empathy are very important to me. I’ll continue developing—learning new skills while honing existing ones—and I’ll share everything I learn.</p>
           </header>
         </div>
       </section>
+
+<div className={css.resume.container}>
+<div className={css.resume.col6}>
 
       <section id="work" className={css.resume.work}>
         <div className={css.resume.wrapper}>
@@ -39,13 +40,12 @@ class Resume extends React.Component {
           <div className={css.resume.block}>
             <div className={css.resume.flex}>
               <h2>Xamarin</h2>
-              <date>2014-03-17—</date>
+              <date>2014-03-17—Present</date>
             </div>
             <div>Full Stack Developer</div>
             <div>
               <p>Xamarin provides development tools to build and test native mobile apps, including Xamarin Platform, Xamarin Test Cloud, &amp; Xamarin Insights.</p>
             </div>
-            <h3>Highlights</h3>
             <ul>
               <li>Wrote 190,000 lines of production code on <a href="https://xamarin.com">xamarin.com</a>.</li>
               <li>Converted an additional 20,000 lines of Less to Sass.</li>
@@ -65,7 +65,6 @@ class Resume extends React.Component {
               <date>2014-01-13 — 2014-05-02</date>
             </div>
             <div>Web Developer</div>
-            <h3>Highlights</h3>
             <ul>
               <li>Designed and developed <a href="http://auburn.edu/career/">responsive websites</a> with the Laravel MVC framework.</li>
               <li>Integrated feeds from Twitter, the University Calendar and the Career Center Blog.</li>
@@ -80,7 +79,6 @@ class Resume extends React.Component {
             <div>
               <p>The ACLU is our nation's guardian of liberty, working daily in courts, legislatures and communities to defend and preserve the individual rights and liberties.</p>
             </div>
-            <h3>Highlights</h3>
             <ul>
             <li>Designed, developed and published a <a href="http://aclualabama.org/wp/">WordPress-based website</a>.</li>
             <li>Delivered comprehensive documentation.</li>
@@ -95,7 +93,6 @@ class Resume extends React.Component {
             <div>
               <p>The Auburn University Office of Information Technology provides centralized computing services and resources to the Auburn University community.</p>
             </div>
-            <h3>Highlights</h3>
             <ul>
               <li>Maintained the Auburn University <a href="http://auburn.edu">homepage</a>.</li>
               <li>Created a <a href="http://www.auburn.edu/asim">science education website</a> providing resources to twelve universities and every high school in Alabama.</li>
@@ -114,14 +111,40 @@ class Resume extends React.Component {
               <h2>Auburn University</h2>
               <date>2010-08-18 — 2014-12-13</date>
             </div>
-            <div>BS in Software Engineering; Spanish concentration</div>
-            <h3>Courses</h3>
-            <ul className={css.resume.courses}>
-              <li>COMP 4970 - Introduction To Game Design and Development</li>
-              <li>COMP 5600 - Artificial Intelligence</li>
-              <li>COMP 5710 - Software Quality Assurance</li>
-            </ul>
+            <div className={css.resume.flex}>
+              <span>BS in Software Engineering; Spanish concentration</span>
+              <span>3.28 GPA</span>
+            </div>
           </div>
+        </div>
+      </section>
+
+</div>
+<div className={css.resume.col6}>
+
+      <section id="skills" className={css.resume.skills}>
+        <div className={css.resume.wrapper}>
+            <h1>Skills.</h1>
+            <ol className={css.resume.ol}>
+              <li>Create design comps and icons with <strong>Sketch</strong>. Manipulate complex paths in <strong>Illustrator</strong>. Perform raster image edits in <strong>Photoshop</strong>.</li>
+              <li>Write semantic <strong>HTML5</strong> and <strong>JSX</strong> including ARIA landmark roles</li>
+              <li>Use CSS Modules to create composable style classes. Add variables and prefixes with <strong>PostCSS</strong>, <strong>Sass</strong> or <strong>Less</strong>.</li>
+              <li>Build and transpile <strong>ECMAScript 6/7</strong> modules with Babel. Lint with ESLint. Validate types with Flow.</li>
+              <li>Reduce boilerplate by using a framework like <strong>React</strong> (+ Redux) or <strong>Ember</strong>.</li>
+              <li>Produce optimized JPEG 2000 and WebP images with a fallback. Serve fingerprinted static assets with long Cache-Control headers.</li>
+              <li>Remove unused glyphs in <strong>FontForge</strong> while preserving OpenType features.</li>
+              <li>Compile assets with <strong>Webpack</strong>, <strong>Grunt</strong> or <strong>Broccoli</strong>.</li>
+              <li>Make sites available offline via an <strong>App Cache</strong> and a <strong>Service Worker</strong>.</li>
+              <li>Set up a web server with <strong>Node.js</strong> &amp; <strong>Express</strong>. Configure <strong>NGINX</strong> as a reverse proxy, load balancer or static file server with HTTP/2 support.</li>
+              <li>Store data in <strong>MongoDB</strong> and <strong>Redis</strong>. Spin up <strong>MariaDB</strong> or <strong>Postgres</strong> if I’m using Ghost or <strong>WordPress</strong>.</li>
+              <li>Manage environments with <strong>Docker</strong> Machine and Compose. Manage code with <strong>Git</strong>.</li>
+              <li>Set up <strong>Amazon Web Services</strong> (IAM, EC2, RDS, SES, SNS, S3, CloudFront, Route 53).</li>
+              <li>Purchase a domain and configure DNS. Add DKIM, SPF and DMARC records to verify emails. Set up secondary DNS with AXFR.</li>
+              <li>Install an SSL certificate, then configure the server for HSTS, HPKP, forward secrecy and OCSP stapling. Generate unique primes for DH.</li>
+              <li>Test via <strong>Mocha</strong>, Chai and <strong>Sinon</strong>. Report coverage to Code Climate and Coveralls. Run continuously in Travis CI and AppVeyor.</li>
+              <li>Manage depedency security with Snyk, compatibility with Greenkeeper and updates with David DM.</li>
+              <li>Analyze traffic in <strong>Google Analytics</strong>. Monitor servers and processes with <strong>New Relic</strong>.</li>
+            </ol>
         </div>
       </section>
 
@@ -138,59 +161,32 @@ class Resume extends React.Component {
         </div>
       </section>
 
-      <section id="skills" className={css.resume.skills}>
-        <div className={css.resume.wrapper}>
-            <h1>Skills.</h1>
-            <ol className={css.resume.ol}>
-              <li><strong>Sketch</strong> to create design comps and icons. <strong>Illustrator</strong> for more complex paths. <strong>Photoshop</strong> for raster image edits.</li>
-              <li>Semantic <strong>HTML5</strong> or <strong>JSX</strong> with ARIA landmark roles</li>
-              <li>CSS Modules for composable style classes. <strong>PostCSS</strong> for variables and prefixes.  <strong>Sass</strong>/<strong>Less</strong> in older projects. Color contrast checked in tota11y.</li>
-              <li><strong>ECMAScript 6/7</strong> transpiled with Babel. Organized in modules. Linted with ESLint. Types via Flow.</li>
-              <li>If necessary, a framework to organize routing, data, etc. Lately I’m working with <strong>React</strong> + Redux + React Router, but I’ve used <strong>Ember</strong> too.</li>
-              <li>JPEG 2000 and WebP images with a fallback. Optimized and fingerprinted. Sent with long Cache-Control headers.</li>
-              <li><strong>FontForge</strong> to remove unused glyphs from web fonts while preserving OpenType features.</li>
-              <li>Build pipeline via <strong>Webpack</strong>. Hot reloading of styles, components and routes. I’ve also used <strong>Grunt</strong> and <strong>Broccoli</strong>.</li>
-              <li>Offline and low-bandwidth support via both <strong>App Cache</strong> and a <strong>Service Worker</strong>.</li>
-              <li><strong>Node.js</strong> + <strong>Express</strong> as a web server. <strong>NGINX</strong> as a reverse proxy, load balancer or static file server. With HTTP/2.</li>
-              <li><strong>MongoDB</strong> and <strong>Redis</strong> for data storage. <strong>MariaDB</strong> or <strong>Postgres</strong> if I’m setting up Ghost or <strong>WordPress</strong>.</li>
-              <li><strong>Docker</strong> Machine and Compose to manage environments. <strong>Git</strong>’s reflog to save my lost code.</li>
-              <li><strong>Amazon Web Services</strong> (IAM, EC2, RDS, SES, SNS, S3, CloudFront, Route 53) as a PaaS.</li>
-              <li>Domain purchased and DNS configured. DKIM, SPF and DMARC records for email verification. Secondary DNS with AXFR.</li>
-              <li>SSL certificate installed and server configured for HSTS, HPKP, forward secrecy and OCSP stapling. Unique primes for DH.</li>
-              <li>Tests via <strong>Mocha</strong>, Chai and <strong>Sinon</strong>. Coverage reported to Code Climate and Coveralls. Run continuously in Travis CI and AppVeyor.</li>
-              <li>Dependencies managed via Snyk (for security), Greenkeeper (for safety) and David DM (for updates).</li>
-              <li>Visitor analytics through <strong>Google Analytics</strong>. Server and process monitoring in <strong>New Relic</strong>.</li>
-            </ol>
-        </div>
-      </section>
-
-      <section className={css.home.explore} id='play'>
-        <div className={css.resume.wrapper}>
-          <h1>Interests.</h1>
-          <p>I like engaging with brands on Twitter, savoring smoky whisky, creating Jackson Pollock-esque latte art, geocaching in the great outdoors, comparing Sortino ratios, memorizing Kanji, practing yoga, jamming to freak folk, critiquing postmodern literature, learning about intersectionality and staring thoughtfully at Impressionist paintings.</p>
-        </div>
-      </section>
-
-      <section className={css.home.connect} id='connect'>
+      <section className={css.resume.profiles} id='connect'>
         <div className={css.app.wrapper}>
-          <h1>Profiles.</h1>
           <div className={css.home.connectIcons}>
             <a className={css.home.twitterLink} href='https://twitter.com/smockled' title='Twitter'>
               <img alt='Twitter' className={css.home.twitterIcon} src={require('../assets/images/connect/twitter.svg')}></img>
+              @smockled
             </a>
             <a className={css.home.githubLink} href='https://github.com/smockle' title='GitHub'>
               <img alt='GitHub' className={css.home.gitHubIcon} src={require('../assets/images/connect/github.svg')}></img>
+              smockle
             </a>
             <a className={css.home.dribbbleLink} href='https://dribbble.com/smockle' title='Dribbble'>
               <img alt='Dribbble' className={css.home.dribbbleIcon} src={require('../assets/images/connect/dribbble.svg')}></img>
+              smockle
             </a>
             <a className={css.home.linkedInLink} href='https://linkedin.com/in/smockle' title='LinkedIn'>
               <img alt='LinkedIn' className={css.home.linkedInIcon} src={require('../assets/images/connect/linkedin.svg')}></img>
+              smockle
             </a>
           </div>
         </div>
       </section>
-    </div>
+</div>
+</div>
+
+</div>
     );
   }
 }

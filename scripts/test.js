bb99566555d625e8e34a5d37cba2a58561a71674
Clay Miller <clay@smockle.com>
2018-03-06 06:16:32 -0800

    Validate AMP in 'npm test' task

---
+++ scripts/test.js
@@ -0,0 +1,20 @@
+const amphtmlValidator = require("amphtml-validator");
+const fs = require("fs");
+const path = require("path");
+
+amphtmlValidator.getInstance().then(validator => {
+  const input = fs.readFileSync(
+    path.join(__dirname, "../public/index.amp.html"),
+    "utf8"
+  );
+  const result = validator.validateString(input);
+  (result.status === "PASS" ? console.log : console.error)(result.status);
+  for (var ii = 0; ii < result.errors.length; ii++) {
+    var error = result.errors[ii];
+    var msg = `line ${error.line}, col ${error.col}: ${
+      error.message
+    }${error.specUrl !== null && ` (see ${error.specUrl})`}`;
+    (error.severity === "ERROR" ? console.error : console.warn)(msg);
+  }
+  process.exit(result.status !== "PASS" ? 1 : 0);
+});

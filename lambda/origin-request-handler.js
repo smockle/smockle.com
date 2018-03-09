8d5c1f6177cde4a6e32ff8e195558cf47e5ed27b
Clay Miller <clay@smockle.com>
2018-03-08 18:46:27 -0800

    Add AWS Lambda origin request & viewer request functions to serve compressed files

---
+++ lambda/origin-request-handler.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/** Function registered on 'Origin Request' CloudFront Event */
+exports.handler = (event, context, callback) => {
+  const request = event.Records[0].cf.request;
+  const headers = request.headers;
+
+  /** Map supported compression algorithms to file extensions */
+  const extensions = new Map([["br", ".br"], ["gzip", ".gz"]]);
+
+  /** @type {string | null} Value of custom "X-COMPRESSION" HTTP header */
+  const header = headers["x-compression"] && headers["x-compression"][0].value;
+
+  // Append file extension to request URI
+  if (header && extensions.get(header)) {
+    request.uri += extensions.get(header);
+  }
+
+  callback(null, request);
+};

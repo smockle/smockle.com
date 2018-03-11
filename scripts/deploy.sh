ee7e5bfd754b8a64b7ecb3ffd40b7a71ec444a9a
Clay Miller <clay@smockle.com>
2018-03-11 14:40:34 -0700

    Add 'charset=utf-8' to CSS & HTML. Don’t set 'minimum-scale' in HTML viewport meta tag. Increase 'Cache-Control' 'max-age' and set 'immutable'.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,19 +10,24 @@ SOURCE=$1
 TARGET=$2
 
 # Sync other files
-aws s3 sync "${SOURCE}" "${TARGET}" --region us-west-2 --acl public-read --cache-control 'max-age=2629000' --exclude '*.html' --exclude '*.svg' --exclude '*.ico'
+aws s3 sync "${SOURCE}" "${TARGET}" --region us-west-2 --acl public-read --cache-control 'max-age=31536000, immutable' --exclude '*.svg' --exclude '*.ico' --exclude '*.css' --exclude '*.html'
 
 # Sync SVG files
 # + 'Content-Type: image/svg+xml; charset=utf-8'
-aws s3 sync "${SOURCE}" "${TARGET}" --region us-west-2 --acl public-read --cache-control 'max-age=2629000' --content-type 'image/svg+xml; charset=utf-8' --exclude '*' --include '*.svg'
+aws s3 sync "${SOURCE}" "${TARGET}" --region us-west-2 --acl public-read --cache-control 'max-age=31536000, immutable' --content-type 'image/svg+xml; charset=utf-8' --exclude '*' --include '*.svg'
 
 # Sync ICO files (favicon)
 # + 'Content-Type: image/x-icon'
-aws s3 sync "${SOURCE}" "${TARGET}" --region us-west-2 --acl public-read --cache-control 'max-age=2629000' --content-type 'image/x-icon' --exclude '*' --include '*.ico'
+aws s3 sync "${SOURCE}" "${TARGET}" --region us-west-2 --acl public-read --cache-control 'max-age=31536000, immutable' --content-type 'image/x-icon' --exclude '*' --include '*.ico'
+
+# Sync CSS files
+# + 'Content-Type: text/css; charset=utf-8'
+aws s3 sync "${SOURCE}" "${TARGET}" --region us-west-2 --acl public-read --cache-control 'max-age=31536000, immutable' --content-type='text/css; charset=utf-8' --exclude '*' --include '*.css'
 
 # Sync HTML files
 # + 'Cache-Control: public, must-revalidate, proxy-revalidate, max-age=0'
-aws s3 sync "${SOURCE}" "${TARGET}" --region us-west-2 --acl public-read --cache-control 'public, must-revalidate, proxy-revalidate, max-age=0' --exclude '*' --include '*.html'
+# + 'Content-Type: text/html; charset=utf-8'
+aws s3 sync "${SOURCE}" "${TARGET}" --region us-west-2 --acl public-read --cache-control 'public, must-revalidate, proxy-revalidate, max-age=0' --content-type='text/html; charset=utf-8' --exclude '*' --include '*.html'
 
 unset -v SOURCE
 unset -v TARGET

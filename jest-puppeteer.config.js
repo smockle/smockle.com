28033b35b06ee788df5ec31d1bcc4540f2ee4520
Clay Miller <clay@smockle.com>
2019-04-28 17:44:59 -0700

    Use local tests

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,8 @@
 const servers = {
   development: {
     server: {
-      command: "npm run build && npm run serve",
-      port: 9000,
+      command: "npm start && npm run serve",
+      port: 8000,
       launchTimeout: 20000
     }
   }

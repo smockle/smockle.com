41da89793c524b82e3a4b2916793254da9f7de54
Clay Miller <clay@smockle.com>
2019-09-13 00:05:11 -0400

    Update packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,9 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: true
   },
-  extends: `react-app`,
+  // extends: "react-app",
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint", "@typescript-eslint/tslint"],
+  plugins: ["@typescript-eslint"],
   parserOptions: {
     project: "tsconfig.json",
     ecmaVersion: 2018,
